9f8ce247266e1ab2a2075845cd50fc0b59e94cdb
Soulter <905617992@qq.com>
2026-04-23 22:28:40 +0800

    chore: bump version to 4.23.5

--- astrbot/core/config/default.py
+++ astrbot/core/config/default.py
@@ -5,7 +5,7 @@ from typing import Any, TypedDict
 
 from astrbot.core.utils.astrbot_path import get_astrbot_data_path
 
-VERSION = "4.23.4"
+VERSION = "4.23.5"
 DB_PATH = os.path.join(get_astrbot_data_path(), "data_v4.db")
 PERSONAL_WECHAT_CONFIG_METADATA = {
     "weixin_oc_base_url": {
